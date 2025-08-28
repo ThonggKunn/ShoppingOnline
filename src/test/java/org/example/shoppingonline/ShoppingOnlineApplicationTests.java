e539cd7582128d59dd61bb4aad4474797f87c132
Dương Minh Thông <153206471+ThonggKunn@users.noreply.github.com>
2025-08-28 21:27:29 +0700

    feat: set up initial project

---
+++ src/test/java/org/example/shoppingonline/ShoppingOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
